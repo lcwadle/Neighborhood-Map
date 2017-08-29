72134cdb7a1ae0de0f1ef8a6f3ed817c2d21c49e
Lawrence Wadle <lcwadle@longhornhealth.com>
2017-08-29 16:48:43 -0500

    Added filtering capability

--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@ var largeInfoWindow;
 
 // Business Object
 var Business = function(rating, price, phone, id, review_count, name,
-    lattitude, longitude, image_url) {
+    lattitude, longitude, image_url, marker) {
   this.rating = rating;
   this.price = price;
   this.phone = phone;
@@ -16,12 +16,33 @@ var Business = function(rating, price, phone, id, review_count, name,
   this.latitude = latitude;
   this.longitude = longitude;
   this.image_url = image_url;
+  this.marker = marker;
+  this.displayed = ko.observable(true);
 }
 
 // View Model
 var viewModel = {
   businesses: ko.observableArray([])
 };
+
+viewModel.addBusiness = function(business) {
+  viewModel.businesses.push(business);
+}
+
+viewModel.showBusinessesbyRating = function(stars) {
+  for (var i = 0; i < viewModel.businesses().length; i++) {
+    if (stars == 0) {
+      viewModel.businesses()[i].marker.setMap(map);
+      viewModel.businesses()[i].displayed(true);
+    } else if (viewModel.businesses()[i].rating >= stars && viewModel.businesses()[i].rating < stars + 1) {
+      viewModel.businesses()[i].marker.setMap(map);
+      viewModel.businesses()[i].displayed(true);
+    } else {
+      viewModel.businesses()[i].marker.setMap(null);
+      viewModel.businesses()[i].displayed(false);
+    }
+  }
+}
 
 // Yelp POST URL Paramaters
 var yelp_post_url = "https://api.yelp.com/oauth2/token";
@@ -36,16 +57,6 @@ yelp_post_url = yelp_post_url + "&client_secret=" + client_secret;
 
 // Yelp Access token
 var access_token = "w7-3rcA8zqaBpMU8e8iLJObLV_QQVtItNsP5dug3Cyhw6ml79N74hkPu3AjneQelrBw852xfQpo3zJbZKgKaflZy1Ya3NBfPaj0FT4ltN3_8_s2H-mRyK4l0J7qkWXYx";
-
-/*
-// Yelp Access Token
-$.post({
-  url: yelp_post_url,
-  success: function(data) {
-    access_token = data.access_token;
-  }
-});
-*/
 
 // Yelp GET URL Paramaters
 var yelp_get_url = "https://cors-anywhere.herokuapp.com/" + "https://api.yelp.com/v3/businesses/search";
@@ -65,23 +76,28 @@ $.ajax({
   headers: {'Authorization': "Bearer w7-3rcA8zqaBpMU8e8iLJObLV_QQVtItNsP5dug3Cyhw6ml79N74hkPu3AjneQelrBw852xfQpo3zJbZKgKaflZy1Ya3NBfPaj0FT4ltN3_8_s2H-mRyK4l0J7qkWXYx"},
   success: function(data) {
     $.each(data.businesses, function (index, value) {
-      viewModel.businesses.push(new Business(value.rating, value.price,
-        value.phone, value.id, value.review_count, value.name, value.coordinates.latitude,
-        value.coordinates.longitude, value.image_url));
-
       var marker = new google.maps.Marker({
           map: map,
           position: { lat: value.coordinates.latitude, lng: value.coordinates.longitude },
           title: value.name,
           animation: google.maps.Animation.DROP,
-          id: value.id
+          id: value.id,
+          image_url: value.image_url,
+          rating: value.rating,
+          price: value.price,
+          phone: value.phone,
+          review_count: value.review_count
         });
 
-      markers.push(marker);
+        viewModel.addBusiness(new Business(value.rating, value.price,
+          value.phone, value.id, value.review_count, value.name, value.coordinates.latitude,
+          value.coordinates.longitude, value.image_url, marker));
+
       bounds.extend(marker.position);
       marker.setMap(map);
 
       marker.addListener('click', function() {
+            this.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE));
             populateInfoWindow(this, largeInfoWindow);
           });
     });
@@ -95,7 +111,16 @@ function populateInfoWindow(marker, infowindow) {
         // Check to make sure the infowindow is not already opened on this marker.
         if (infowindow.marker != marker) {
           infowindow.marker = marker;
-          infowindow.setContent('<div>' + marker.title + '</div>');
+          infowindow.setContent(
+            "<div class='infoWindow'>" +
+              '<h2>' + marker.title + '</h2>' +
+              '<img src=' + marker.image_url + ' />' +
+              '<h4> Phone: ' + marker.phone + '</h4>' +
+              '<h4> Price: ' + marker.price + '</h4>' +
+              '<h4> Rating: ' + marker.rating +
+                "<i class='fa fa-star'><i>" + '</h4>' +
+            '</div>'
+          );
           infowindow.open(map, marker);
           // Make sure the marker property is cleared if the infowindow is closed.
           infowindow.addListener('closeclick', function() {
